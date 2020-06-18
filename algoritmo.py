feb5a89ff2a09cc2e04aefd1b66000f10a40587d
Nicolás Andrews Sandoval <nm.andrewss@gmail.coM>
2020-06-17 21:15:35 -0400

    postulantes por max postulacion; fix xlsx

--- algoritmo.py
+++ algoritmo.py
@@ -1,5 +1,12 @@
 import xlsxwriter
 
+# Informacion
+archivo_grupos = "grupos.csv"
+archivo_carreras = "carreras.csv"
+
+# Nombre de los archivos (debug)
+archivo_datos = "ejemplo.csv"
+
 def AgregarDePana(Bucket, valor, gnumber):
     # Flag de si entró el valor o no.
     flag = False
@@ -8,22 +15,22 @@ def AgregarDePana(Bucket, valor, gnumber):
     if length == 0:
         Bucket[gnumber].append(valor)
     else:
-        for v in range(len(Bucket[gnumber])):
+        for v in range(length):
             if valor[1] > Bucket[grupom][v][1]:
                 Bucket[grupom].insert(v, valor)
                 flag = True
                 break
-        # Si no ha entrado el valor, y el arreglo en la bucket es menor al máximo de vacantes, entonces lo agregamos al final
-        if(not flag and len(Bucket[gnumber]) < grupos[gnumber-1][2]):
+        # Si no ha entrado el valor, y el arreglo en la bucket es menor al máximo de vacantes por grupo, entonces lo agregamos al final
+        if(not flag and length < grupos[gnumber-1][2]):
             Bucket[gnumber].append(valor)
     
     # Revisamos si el diccionario modificado tiene más vacantes de lo disponible, eliminamos el último
-    if length+1 > grupos[gnumber-1][6]:
+    if length+1 >= grupos[gnumber-1][6]:
         del Bucket[gnumber][-1]
 
 #Cargar los grupos de carreras agrupados por ponderaciones.
 grupos = []
-file = open("grupos.csv", "r")
+file = open(archivo_grupos , "r")
 for line in file:
     string = line.split(",")
     for i in range(1,8):
@@ -36,7 +43,7 @@ file.close()
 
 #Cargar los nombres de las carreras
 carreras = []
-file = open("carreras.csv", "r")
+file = open(archivo_carreras, "r")
 for line in file:
     string = line.split(",")
     for i in range(1,3):
@@ -47,11 +54,8 @@ file.close()
 #Parsear postulantes
 buckets = { 1:[], 2:[], 3:[], 4:[], 5:[], 6:[], 7:[], 8:[], 9:[], 10:[], 11:[], 12:[]}
 datos = []
-file = open("mini.csv", "r")
-grupom = 1
+file = open(archivo_datos, "r")
 for line in file:
-    if(grupom > 12):
-        grupom = 1
     string = line.split(";")
     # Pasar a int
     for i in range(7):
@@ -67,16 +71,44 @@ for line in file:
         del string[5]
 
     # Ponderar
-    suma = 0
-    for i in range(1,6):
-        suma += string[i] * grupos[grupom-1][i]
-        maximo = round(suma, 2)
+    maximo = 0
+    grupom = 0
+    for g in grupos:
+        suma = 0
+        for i in range(1,6):
+            suma += string[i] * g[i]
+        if(suma > maximo):
+            maximo = suma
+            grupom = g[-1]
     valor.append(maximo) # Agregar el puntaje ponderado maximo
     # Funcion auxiliar: agregar de pana. (Ordenados)
-    ##debug| buckets[grupom].append(valor)
+    #buckets[grupom].append(valor)
     AgregarDePana(buckets, valor, grupom)
-    grupom = grupom + 1
 
 file.close()
 print(buckets)
 
+#def EscribirExcel:
+excel = xlsxwriter.Workbook('holiwi.xlsx')
+for item in carreras:
+    worksheets = []
+    worksheets.append(excel.add_worksheet(item[0]))
+    row = 0
+    contador = 0
+    col = 0
+    vacantes = item[-2]
+    lista = list(buckets[item[-1]])
+    for b in range(len(lista)): # El grupo al que pertenecen las carreras está al final; V es la tupla
+        if(row == item[1]):
+            break
+        worksheets[-1].write(row, col, lista[0][0]) # Debemos acceder al ultimo worksheet; y al primer elemento de la lista (mayor) 
+        worksheets[-1].write(row, col+1, lista[0][1]) # 0 y 1 son rut y ptje ponderado
+        # Eliminamos el primer elemento y avanzamos el puntero
+        row += 1
+        del lista[0]
+        contador += 1
+    # También debemos sacar los elementos de la bucket
+    del buckets[item[-1]][:contador]
+
+
+excel.close()
