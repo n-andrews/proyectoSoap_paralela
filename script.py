524c8048ea89d15ec5a139c199391b5d80df918a
n-andrews <26151604+n-andrews@users.noreply.github.com>
2020-08-01 22:39:05 -0400

    Delete script.py

--- script.py
+++
@@ -1,9 +0,0 @@
-import sys
-input_file = str(sys.argv[1])
-
-import base64
-data = open(input_file, 'rb').read()
-base64_encoded = base64.b64encode(data)
-file = open("output.bin", 'wb').write(base64_encoded)
-
-## Este script sirve para crear un archivo que es un string de b64 para simular el request sin tener que montar el srever y usar soap ui pq vale callampa
